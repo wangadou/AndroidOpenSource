23a8bcff1965f2d00504f65fd69ef76355ca8368
leaderbean <634051075@qq.com>
2017-05-11 18:14:25 +0800

    提交模块代码

---
+++ mylibrary/src/main/java/com/wwb/library/fragment/DDBaseFragment.java
@@ -0,0 +1,21 @@
+package com.wwb.library.fragment;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by wwb on 2017/5/11.
+ */
+
+public class DDBaseFragment extends Fragment implements OnClickListener
+{
+
+
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
---
+++ mylibrary/src/main/java/com/wwb/library/manager/DDFragmentManager.java
@@ -0,0 +1,12 @@
+package com.wwb.library.manager;
+
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by wwb on 2017/5/11.
+ */
+
+public class DDFragmentManager
+{
+    private FragmentManager mManager;
+}
